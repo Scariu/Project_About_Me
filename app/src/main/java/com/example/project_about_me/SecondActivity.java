3c36157aefa7063e74dd7617ec6225d6ed9a0220
Flopy <f.hidalgoalarcon@gmail.com>
2023-06-29 21:09:47 -0400

    Se crea diseño de SecondActivity y se agregan funciones a botones.

--- app/src/main/java/com/example/project_about_me/SecondActivity.java
+++ app/src/main/java/com/example/project_about_me/SecondActivity.java
@@ -2,7 +2,10 @@ package com.example.project_about_me;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.project_about_me.databinding.ActivityMainBinding;
 import com.example.project_about_me.databinding.ActivitySecondBinding;
@@ -14,5 +17,46 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivitySecondBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        initListeners();
+    }
+
+    private void initListeners() {
+        binding.buttonLinkedin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentLinke = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.linkedin.com//in/florencia-hidalgo-83315a1ab"));
+                startActivity(intentLinke);
+            }
+        });
+        binding.buttonWhat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentWhat = new Intent(Intent.ACTION_VIEW, Uri.parse("https://api.whatsapp.com/send?phone=+56990561282"));
+                startActivity(intentWhat);
+            }
+        });
+        binding.buttonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendEmail();
+            }
+        });
+
+    }
+
+
+    protected void sendEmail() {
+        String[] TO = {"f.hidalgoalarcon@gmail.com"};
+        String[] CC = {""};
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setData(Uri.parse("mail"));
+        emailIntent.setType("text/plain");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+        emailIntent.putExtra(Intent.EXTRA_CC, CC);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Your subject");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "Email message");
+        startActivity(Intent.createChooser(emailIntent, "Send Mail"));
+        finish();
+
     }
 }
